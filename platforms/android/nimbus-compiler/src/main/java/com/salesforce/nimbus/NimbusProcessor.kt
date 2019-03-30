bd9a082fafa580dfa6f2f93e53f149dad4a0cd02
Jason Foreman <jason@threeve.org>
2019-03-30 15:17:41 -0500

    WIP on JSON args...

--- platforms/android/nimbus-compiler/src/main/java/com/salesforce/nimbus/NimbusProcessor.kt
+++ platforms/android/nimbus-compiler/src/main/java/com/salesforce/nimbus/NimbusProcessor.kt
@@ -41,30 +41,49 @@ class NimbusProcessor: AbstractProcessor() {
                                         .build())
                         .addModifiers(Modifier.PUBLIC)
                         .returns(TypeName.get(methodElement.returnType))
+                        .addException(ClassName.get("org.json", "JSONException"))
+
+                methodSpec.addParameter(String::class.java, "argString")
+
+                val jsonObject = ClassName.get("org.json", "JSONArray")
+                methodSpec.addStatement("\$T args = new \$T(argString)", jsonObject, jsonObject)
 
                 val arguments = mutableListOf<String>()
+                var argIndex = 0
 
                 methodElement.parameters.forEach {
 
                     // check if param needs conversion
-//                    when(it.asType().kind) {
-//                        TypeKind.DECLARED -> print("")
-//                        else -> {
-                            methodSpec.addParameter(TypeName.get(it.asType()), it.simpleName.toString())
+                    when(it.asType().kind) {
+                        TypeKind.BOOLEAN -> methodSpec.addStatement("\$T \$N = args.getBoolean($argIndex)", it.asType(), it.simpleName)
+                        TypeKind.INT -> methodSpec.addStatement("\$T \$N = args.getInt($argIndex)", it.asType(), it.simpleName)
+                        TypeKind.DOUBLE -> methodSpec.addStatement("\$T \$N = args.getDouble($argIndex)", it.asType(), it.simpleName)
+                        TypeKind.FLOAT -> methodSpec.addStatement("\$T \$N = args.getDouble($argIndex)", it.asType(), it.simpleName)
+                        TypeKind.LONG -> methodSpec.addStatement("\$T \$N = args.getLong($argIndex)", it.asType(), it.simpleName)
+                        TypeKind.DECLARED -> {
+                            // TODO:
+                            if (it.asType().toString().equals("java.lang.String")) {
+                                methodSpec.addStatement("\$T \$N = args.getString($argIndex)", it.asType(), it.simpleName)
+                            } else {
+                                methodSpec.addStatement("\$T \$N = null", it.asType(), it.simpleName)
 
-//                        }
-//                    }
+                            }
+                        }
+                        else -> {
+//                            methodSpec.addParameter(TypeName.get(it.asType()), it.simpleName.toString())
+                            methodSpec.addStatement("\$T \$N = args.get($argIndex)", it.asType(), it.simpleName)
 
-
-
+                        }
+                    }
 
 
                     arguments.add(it.simpleName.toString())
-
+                    argIndex++
                 }
 
+
                 val hasReturn = it.returnType.kind != TypeKind.VOID
-                methodSpec.addCode("${if (hasReturn) "return" else "" } target.\$N(${arguments.joinToString(", ")});\n", it.simpleName.toString())
+                methodSpec.addCode("${if (hasReturn) "return " else "" }target.\$N(${arguments.joinToString(", ")});\n", it.simpleName.toString())
 
                 type.addMethod(methodSpec.build())
             }
