74b44df3b814d21637101d7f2704a06e08f95c81
Paul Tiarks <ptiarks@salesforce.com>
2020-02-10 15:57:12 -0600

    Add function for injecting nimbus js

--- platforms/android/nimbusjs/src/main/java/com/salesforce/nimbusjs/NimbusJSUtilities.kt
+++ platforms/android/nimbusjs/src/main/java/com/salesforce/nimbusjs/NimbusJSUtilities.kt
@@ -1,1 +1,18 @@
 package com.salesforce.nimbusjs
+
+import android.content.Context
+import android.webkit.WebView
+import java.io.BufferedReader
+
+class NimbusJSUtilities() {
+    companion object Injection {
+        fun injectNimbus(webView: WebView, context: Context) {
+            try {
+                val inputStream = context.assets.open("raw/nimbus.js")
+                inputStream.bufferedReader().use(BufferedReader::readText)
+            } catch (e: Exception) {
+                null
+            }?.let { webView.loadUrl("javascript:($it)()")  }
+        }
+    }
+}
