7ca8fb9aedf6249b087a87653c846b8fbcb90bcb
Paul Tiarks <ptiarks@salesforce.com>
2020-02-12 15:00:04 -0600

    Clean up how nimbus js is read out of resources

--- platforms/android/nimbusjs/src/main/java/com/salesforce/nimbusjs/NimbusJSUtilities.kt
+++ platforms/android/nimbusjs/src/main/java/com/salesforce/nimbusjs/NimbusJSUtilities.kt
@@ -8,9 +8,8 @@ import java.nio.charset.StandardCharsets
 
 class NimbusJSUtilities() {
     companion object Injection {
-
         fun injectedNimbusStream(inputStream: InputStream, context: Context): InputStream {
-            val jsString = ResourceUtils(context).stringFromRawResource(R.raw.nimbus)
+            val jsString = context.resources.openRawResource(R.raw.nimbus).bufferedReader(StandardCharsets.UTF_8).readText()
             var html = inputStream.bufferedReader(StandardCharsets.UTF_8).readText()
             if (html!!.contains("<head>")) {
                 html = html.replace("<head>", "<head>\n$jsString\n")
@@ -19,24 +18,5 @@ class NimbusJSUtilities() {
             }
             return ByteArrayInputStream(html!!.toByteArray(StandardCharsets.UTF_8))
         }
-    }
-}
-
-private fun String.Companion.fromStream(stream: InputStream): String {
-    val result = ByteArrayOutputStream()
-    val buffer = ByteArray(1024)
-    var length = stream.read(buffer)
-    while (length != -1) {
-        result.write(buffer, 0, length)
-        length = stream.read(buffer)
-    }
-    return result.toString(StandardCharsets.UTF_8.name())
-}
-
-private class ResourceUtils(val context: Context) {
-    fun stringFromRawResource(id: Int): String {
-        val inputStream = context.resources.openRawResource(id)
-        val string = String.fromStream(inputStream)
-        return string
     }
 }
