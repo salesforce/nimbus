43bc1a5eadd301f54a88b7fe1663601a83060d15
Paul Tiarks <ptiarks@salesforce.com>
2020-02-12 14:14:57 -0600

    Make raw folder in res if it doesn't exist

--- platforms/android/nimbusjs/buildNimbusJS.sh
+++ platforms/android/nimbusjs/buildNimbusJS.sh
@@ -4,6 +4,8 @@ cd ../../packages/nimbus-bridge/
 
 npm run build
 
+mkdir ../../platforms/android/nimbusjs/src/main/res/raw/
+
 cp dist/iife/nimbus.js ../../platforms/android/nimbusjs/src/main/res/raw/nimbus.js
 
 exit 0
