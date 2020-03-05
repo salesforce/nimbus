091d8953aad364c298bd767bae52ec9c7e292734
Takashi Arai <t.arai@salesforce.com>
2020-03-04 23:27:38 -0800

    Add script tag.  Add tests.

--- platforms/android/nimbusjs/src/androidTest/java/com/salesforce/nimbusjs/NimbusJSUtilTests.kt
+++ platforms/android/nimbusjs/src/androidTest/java/com/salesforce/nimbusjs/NimbusJSUtilTests.kt
@@ -1,22 +1,16 @@
 package com.salesforce.nimbusjs
 
 import android.content.Context
-import android.util.Log
-import android.webkit.*
-import androidx.test.internal.runner.junit4.statement.UiThreadStatement.runOnUiThread
+import android.webkit.WebView
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.runner.AndroidJUnit4
 import junit.framework.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.io.*
-import java.net.HttpURLConnection
-import java.net.SocketTimeoutException
-import java.net.URL
+import java.io.InputStream
 import java.nio.charset.StandardCharsets
 import java.util.concurrent.CountDownLatch
-import java.util.concurrent.TimeUnit
 
 
 @RunWith(AndroidJUnit4::class)
@@ -35,11 +29,15 @@ class NimbusJSUtilsTests {
     }
 
     @Test
-    fun testInjectionTwo() {
+    fun testInjection() {
         var inputStream: InputStream = context.assets.open("testPage.html")
         inputStream = NimbusJSUtilities.injectedNimbusStream(inputStream, context)
         var resultString = inputStream.bufferedReader(StandardCharsets.UTF_8).readText()
         var containsNimbus = resultString?.contains("nimbus")
         assertEquals(true, containsNimbus)
+        var containsScriptOpeningTag = resultString?.contains("<script>")
+        assertEquals(true, containsScriptOpeningTag)
+        var containsScriptClosingTag = resultString?.contains("</script>")
+        assertEquals(true, containsScriptClosingTag)
     }
 }
--- platforms/android/nimbusjs/src/main/java/com/salesforce/nimbusjs/NimbusJSUtilities.kt
+++ platforms/android/nimbusjs/src/main/java/com/salesforce/nimbusjs/NimbusJSUtilities.kt
@@ -1,9 +1,8 @@
 package com.salesforce.nimbusjs
 
 import android.content.Context
-import android.util.Log
-import android.webkit.WebView
-import java.io.*
+import java.io.ByteArrayInputStream
+import java.io.InputStream
 import java.nio.charset.StandardCharsets
 
 class NimbusJSUtilities() {
@@ -12,9 +11,9 @@ class NimbusJSUtilities() {
             val jsString = context.resources.openRawResource(R.raw.nimbus).bufferedReader(StandardCharsets.UTF_8).readText()
             var html = inputStream.bufferedReader(StandardCharsets.UTF_8).readText()
             if (html!!.contains("<head>")) {
-                html = html.replace("<head>", "<head>\n$jsString\n")
+                html = html.replace("<head>", "<head>\n<script>\n$jsString\n</script>")
             } else if (html.contains("</head>")) {
-                html = html.replace("</head>", jsString + "\n" + "</head>")
+                html = html.replace("</head>", "<script>\n$jsString\n</script>\n</head>")
             }
             return ByteArrayInputStream(html!!.toByteArray(StandardCharsets.UTF_8))
         }
