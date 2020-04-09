876b6e9653da04845bdebea314f4dc9b25f31371
Paul Tiarks <ptiarks@salesforce.com>
2020-04-09 11:25:58 -0500

    Add empty JSValueEncoderTests

---
+++ platforms/apple/Sources/NimbusTests/JSValueEncoderTests.swift
@@ -0,0 +1,19 @@
+//
+//  JSValueEncoderTests.swift
+//  NimbusTests
+//
+//  Created by Paul Tiarks on 4/9/20.
+//  Copyright © 2020 Salesforce.com, inc. All rights reserved.
+//
+
+import XCTest
+
+class JSValueEncoderTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+}
