6c46ff54741889116d1a66867a8e2643bf4ad9f4
Paul Tiarks <ptiarks@salesforce.com>
2020-04-14 11:24:33 -0500

    Add JSContextConnectionTests

---
+++ platforms/apple/Sources/NimbusTests/JSContextConnectionTests.swift
@@ -0,0 +1,15 @@
+// Copyright (c) 2020, Salesforce.com, inc.
+// All rights reserved.
+// SPDX-License-Identifier: BSD-3-Clause
+// For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+//
+
+import XCTest
+
+class JSContextConnectionTests: XCTestCase {
+
+    override func setUp() {
+
+    }
+
+}
