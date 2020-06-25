62dcd63386870d5ad72a899e07ee09ec0cf130bc
Paul Tiarks <ptiarks@salesforce.com>
2020-06-25 16:24:52 -0500

    Modify implementation to use keypaths instead of enums

--- platforms/apple/Sources/Nimbus/EventTarget.swift
+++ platforms/apple/Sources/Nimbus/EventTarget.swift
@@ -6,46 +6,50 @@
 // root or https://opensource.org/licenses/BSD-3-Clause
 //
 
-protocol Event: Codable {
-    associatedtype EventType: RawRepresentable where EventType.RawValue == String
-    var type: EventType { get }
+import Foundation
+
+protocol EventKeyPathing {
+    static func stringForKeyPath(_ keyPath: PartialKeyPath<Self>) -> String?
 }
 
-typealias Listener = (Int) -> Void
-typealias ListenerMap = [String: Listener]
+private typealias Listener = (Int) -> Void
+private typealias ListenerMap = [String: Listener]
 
-class EventTarget<T: Event> {
+class EventTarget<Events: EventKeyPathing> {
     private var listeners: [String: ListenerMap] = [:]
-    func addEventListener(name: T, listener: @escaping (Int) -> Void) -> String {
+
+    func addEventListener(name: String, listener: @escaping (Int) -> Void) -> String {
         let listenerId = UUID().uuidString
         var listenerMap: ListenerMap = [:]
-        if let map = listeners[name.type.rawValue] {
+        if let map = listeners[name] {
             listenerMap = map
         }
         listenerMap[listenerId] = listener
-        listeners[name.type.rawValue] = listenerMap
+        listeners[name] = listenerMap
         return listenerId
     }
 
     func removeEventListener(listenerId: String) {
-        listeners.forEach { (key, map) in
-            listeners[key] = map.filter { (key, _) in
-                return key != listenerId
+        listeners.forEach { key, map in
+            listeners[key] = map.filter { key, _ in
+                key != listenerId
             }
         }
     }
 
-    func dispatchEvent(event: String) {
-        if let map = listeners[event] {
-            map.forEach { (_, listener) in
-                listener(0)
-            }
+    func publishEvent<V: Encodable>(_ eventKeyPath: KeyPath<Events, V>, payload: V) {
+        guard let eventName = Events.stringForKeyPath(eventKeyPath),
+            let map = listeners[eventName] else {
+            return
+        }
+
+        map.forEach { _, listener in
+            listener(0)
         }
     }
 
     func bind(to connection: Connection) {
         connection.bind(addEventListener, as: "addEventListener")
         connection.bind(removeEventListener, as: "removeEventListener")
-        connection.bind(dispatchEvent, as: "dispatchEvent")
     }
 }
--- platforms/apple/Sources/NimbusTests/EventTargetTests.swift
+++ platforms/apple/Sources/NimbusTests/EventTargetTests.swift
@@ -11,53 +11,68 @@ import XCTest
 @testable import Nimbus
 
 class EventTargetTests: XCTestCase {
-    var target = EventTarget<TestEvent>()
+    var target = EventTarget<TestEvents>()
 
     override func setUp() {
-        target = EventTarget<TestEvent>()
+        target = EventTarget<TestEvents>()
     }
 
-    enum TestEventType: String, Codable {
-        case one
-        case two
+    struct TestEventOne: Codable {
+        var message: String
     }
 
-    struct TestEvent: Event, Codable {
-        var type: TestEventType
+    struct TestEventTwo: Codable {
+        var thingOne: Int
+        var thingTwo: Bool
+    }
+
+    struct TestEvents: EventKeyPathing {
+        var testEventOne: TestEventOne
+        var testEventTwo: TestEventTwo
+        var testEventThree: String
+
+        static func stringForKeyPath(_ keyPath: PartialKeyPath<Self>) -> String? {
+            switch keyPath {
+            case \TestEvents.testEventOne: return "testEventOne"
+            case \TestEvents.testEventTwo: return "testEventTwo"
+            case \TestEvents.testEventThree: return "testEventThree"
+            default:
+                return nil
+            }
+        }
     }
 
     func testBinding() {
         // Verify that EventTarget actually binds its methods to a connection
         let connection = TestConnection(from: JSContext(), bridge: JSContextBridge(), as: "test")
         target.bind(to: connection)
-        XCTAssertEqual(connection.boundNames.count, 3)
+        XCTAssertEqual(connection.boundNames.count, 2)
         XCTAssertTrue(connection.boundNames.contains("addEventListener"))
         XCTAssertTrue(connection.boundNames.contains("removeEventListener"))
-        XCTAssertTrue(connection.boundNames.contains("dispatchEvent"))
     }
 
     func testDispatch() {
         let exp = expectation(description: "dispatch")
-        let event = TestEvent(type: .one)
+        let event = TestEventOne(message: "testMessage")
         // Add a listener
-        _ = target.addEventListener(name: event) { (_) in
+        _ = target.addEventListener(name: "testEventOne") { (_) in
             exp.fulfill()
         }
         // Dispatch an event
-        target.dispatchEvent(event: TestEventType.one.rawValue)
+        target.publishEvent(\.testEventOne, payload: event)
         // verify listener is called
         waitForExpectations(timeout: 1, handler: nil)
     }
 
     func testRemoveListener() {
         var exp = expectation(description: "dispatch")
-        let event = TestEvent(type: .one)
+        let event = TestEventOne(message: "testMessage")
         // Add a listener
-        let listenerId = target.addEventListener(name: event) { (_) in
+        let listenerId = target.addEventListener(name: "testEventOne") { (_) in
             exp.fulfill()
         }
         // Dispatch an event
-        target.dispatchEvent(event: TestEventType.one.rawValue)
+        target.publishEvent(\.testEventOne, payload: event)
         // verify listener is called
         waitForExpectations(timeout: 1, handler: nil)
         // remove listener
@@ -65,7 +80,7 @@ class EventTargetTests: XCTestCase {
         exp = expectation(description: "inverted")
         exp.isInverted = true
         // dispatch event
-        target.dispatchEvent(event: TestEventType.one.rawValue)
+        target.publishEvent(\.testEventOne, payload: event)
         // verify listener is not called
         waitForExpectations(timeout: 2, handler: nil)
     }
