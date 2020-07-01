f7fbdc1d45e97be17437bee24e9de829fddf9142
Paul Tiarks <ptiarks@salesforce.com>
2020-07-01 14:59:15 -0500

    Add documentation to EventPublisher

--- platforms/apple/Sources/Nimbus/EventPublisher.swift
+++ platforms/apple/Sources/Nimbus/EventPublisher.swift
@@ -8,17 +8,34 @@
 
 import Foundation
 
-protocol EventKeyPathing {
+/**
+ Conformers to this protocol provide a mapping from a key path to a string
+ representation of the property name
+ */
+public protocol EventKeyPathing {
     static func stringForKeyPath(_ keyPath: PartialKeyPath<Self>) -> String?
 }
 
 private typealias Listener = (Encodable) -> Void
 private typealias ListenerMap = [String: Listener]
 
-class EventPublisher<Events: EventKeyPathing> {
+/**
+ A mix-in class to add event publishing capabilities to a plugin.
+
+ Instantiate with a type conforming to EventKeyPathing and call
+ this instance's bind function in your plugin's bind, passing in the
+ Connection, and javascript consumers can subscribe/unsubscribe
+ to events in your plugin. Publish events by passing a keyPath with
+ appropriate object as value to send events to listeners of that event.
+ */
+public class EventPublisher<Events: EventKeyPathing> {
     private var listeners: [String: ListenerMap] = [:]
     private var listenerQueue: DispatchQueue = DispatchQueue(label: "EventPublisher")
 
+    /**
+     Adds a listener for the given event name. This function is bound
+     in the bind method and shouldn't be called directly.
+     */
     func addListener(name: String, listener: @escaping (Encodable) -> Void) -> String {
         let listenerId = UUID().uuidString
         listenerQueue.sync {
@@ -29,6 +46,10 @@ class EventPublisher<Events: EventKeyPathing> {
         return listenerId
     }
 
+    /**
+     Removes the given listener so it won't receive future events.
+     This function is bound in the bind method and shouldn't be called directly.
+     */
     func removeListener(listenerId: String) {
         listenerQueue.sync {
             listeners = listeners.mapValues { map in
@@ -39,6 +60,9 @@ class EventPublisher<Events: EventKeyPathing> {
         }
     }
 
+    /**
+     Publishes the given event with the given payload.
+     */
     func publishEvent<V: Codable>(_ eventKeyPath: KeyPath<Events, V>, payload: V) {
         listenerQueue.sync {
             guard let eventName = Events.stringForKeyPath(eventKeyPath),
@@ -51,6 +75,9 @@ class EventPublisher<Events: EventKeyPathing> {
         }
     }
 
+    /**
+     Call this method from your plugins bind method.
+     */
     func bind(to connection: Connection) {
         connection.bind(addListener, as: "addListener")
         connection.bind(removeListener, as: "removeListener")
