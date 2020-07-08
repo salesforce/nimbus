ed0b4a18b82e8a1c50bd1c660c761fc97079e4b6
Jamie Houston <jamie.houston@salesforce.com>
2020-07-08 17:14:31 -0500

    migrated last build.gradle

---
+++ platforms/android/shared-tests/build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("kotlin-kapt")
+    id("kotlinx-serialization")
+}
+
+android {
+    setDefaults()
+
+    sourceSets.getByName("androidTest") {
+        assets.srcDir("../../../packages/test-www/dist")
+        assets.srcDir("../../../packages/nimbus-bridge/dist/iife")
+    }
+
+    packagingOptions {
+        pickFirst("META-INF/LICENSE*")
+        pickFirst("META-INF/DEPENDENCIES")
+    }
+}
+
+dependencies {
+    androidTestImplementation(nimbusModule("annotations"))
+    androidTestImplementation(nimbusModule("bridge-v8"))
+    androidTestImplementation(nimbusModule("bridge-webview"))
+    androidTestImplementation(nimbusModule("core"))
+    kaptAndroidTest(nimbusModule("compiler-webview"))
+    kaptAndroidTest(nimbusModule("compiler-v8"))
+    androidTestImplementation(Libs.androidxTestRules) {
+        exclude("com.android.support", "support-annotations")
+    }
+    androidTestImplementation(Libs.espressoCore)
+    androidTestImplementation(Libs.guava)
+    androidTestImplementation(Libs.j2v8)
+    androidTestImplementation(Libs.junit)
+    androidTestImplementation(Libs.k2v8)
+    androidTestImplementation(Libs.kotlinxSerializationRuntime)
+    androidTestImplementation(Libs.truth)
+}
+
+/*
+ * Compile the test web app prior to assembling the androidTest app
+ */
+val buildTestWebApp = tasks.register<Exec>("buildTestWebApp") {
+    workingDir = File("$rootProject/../../..")
+    commandLine = "npm install".split(" ")
+}
+
+tasks.withType<Assemble>().configureEach {
+    dependsOn(buildTestWebApp)
+}
+
+apply(from = rootProject.file("gradle/lint.gradle"))
