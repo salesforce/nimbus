4be0eab064d5c0488b9d3e330cf528238b054131
Jamie Houston <jamie.houston@salesforce.com>
2020-07-13 15:55:51 -0500

    show results at end

--- platforms/android/gradle/test-output.gradle.kts
+++ platforms/android/gradle/test-output.gradle.kts
@@ -2,11 +2,15 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.gradle.api.tasks.testing.logging.TestLogEvent.*
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 
+// Container for tests summaries
+rootProject.extra.set("testResults", mutableListOf<String>())
+
 allprojects {
     tasks.withType<Test> {
 
         testLogging {
-            events = setOf(FAILED, PASSED, SKIPPED, STANDARD_OUT, STANDARD_ERROR)
+//            events = setOf(FAILED, PASSED, SKIPPED, STANDARD_OUT, STANDARD_ERROR)
+            events = setOf(FAILED, PASSED, SKIPPED, STANDARD_ERROR)
 
             exceptionFormat = TestExceptionFormat.FULL
             showExceptions = true
@@ -21,14 +25,17 @@ allprojects {
             override fun beforeSuite(p0: TestDescriptor?) = Unit
             override fun afterTest(desc: TestDescriptor, result: TestResult) = Unit
             override fun afterSuite(desc: TestDescriptor, result: TestResult) {
-                printResults(desc, result)
+                // Only summarize results for whole modules
+                if (desc.parent == null) {
+                    addResults(desc, result)
+                }
             }
         })
         useJUnitPlatform()
     }
 }
 
-fun printResults(desc: TestDescriptor, result: TestResult) {
+fun addResults(desc: TestDescriptor, result: TestResult) {
     val output = result.run {
         "Results: $resultType (" +
             "$testCount tests, " +
@@ -40,7 +47,19 @@ fun printResults(desc: TestDescriptor, result: TestResult) {
     val testResultLine = "|  $output  |"
     val repeatLength = testResultLine.length
     val seperationLine = "-".repeat(repeatLength)
-    println(seperationLine)
-    println(testResultLine)
-    println(seperationLine)
+//    testResultList.add(testResultLine)
+    val listOfResults = rootProject.extra.get("testResults") as MutableList<String>
+    listOfResults.add(testResultLine)
+    rootProject.extra.set("testResults", listOfResults)
+//    println(seperationLine)
+//    println(testResultLine)
+//    println(seperationLine)
+}
+
+gradle.buildFinished {
+    val allResults = rootProject.extra.get("testResults") as List<String>
+
+    if (allResults.any()) {
+        allResults.forEach{println(it)}
+    }
 }
