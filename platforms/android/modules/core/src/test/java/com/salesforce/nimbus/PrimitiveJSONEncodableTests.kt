445c33f5b18e2c19a8a2ff51278fb22e1594bfe9
Jamie Houston <jamie.houston@salesforce.com>
2020-07-14 11:16:12 -0500

    converted to kotest

--- platforms/android/modules/core/src/test/java/com/salesforce/nimbus/PrimitiveJSONEncodableTests.kt
+++ platforms/android/modules/core/src/test/java/com/salesforce/nimbus/PrimitiveJSONEncodableTests.kt
@@ -9,9 +9,9 @@ package com.salesforce.nimbus
 
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.booleans.shouldBeTrue
 import io.kotest.matchers.shouldBe
 import io.kotest.property.checkAll
-import io.kotest.property.forAll
 import org.json.JSONArray
 import org.json.JSONException
 import org.json.JSONObject
@@ -35,7 +35,7 @@ class PrimitiveJSONEncodableTests : StringSpec({
     }
 
     "listFromJSON<Int>" {
-        forAll<Int, Int, Int> { value1, value2, value3 ->
+        checkAll<Int, Int, Int> { value1, value2, value3 ->
             val json = """
             [
                 $value1,
@@ -45,14 +45,14 @@ class PrimitiveJSONEncodableTests : StringSpec({
             """.trimIndent()
 
             val array = listFromJSON<Int>(json)
-            value1 == array[0] &&
-                value2 == array[1] &&
-                value3 == array[2]
+            value1.shouldBe(array[0])
+            value2.shouldBe(array[1])
+            value3.shouldBe(array[2])
         }
     }
 
     "listFromJSON<Any>" {
-        forAll<String, String, Int> { value1, value2, value3 ->
+        checkAll<String, String, Int> { value1, value2, value3 ->
             val json = """
             [
                 ${JSONObject.quote(value1)},
@@ -62,14 +62,14 @@ class PrimitiveJSONEncodableTests : StringSpec({
             """.trimIndent()
 
             val array = listFromJSON<Any>(json)
-            value1 == array[0] as String &&
-                value2 == array[1] as String &&
-                value3 == array[2] as Int
+            value1.shouldBe(array[0] as String)
+            value2.shouldBe(array[1] as String)
+            value3.shouldBe(array[2] as Int)
         }
     }
 
     "listFromJSON<String> from Int throws" {
-        forAll<String, String, Int> { value1, value2, value3 ->
+        checkAll<String, String, Int> { value1, value2, value3 ->
             val json = """
             [
                 ${JSONObject.quote(value1)},
@@ -81,13 +81,11 @@ class PrimitiveJSONEncodableTests : StringSpec({
             shouldThrow<JSONException> {
                 listFromJSON<String>(json)
             }
-
-            true
         }
     }
 
     "mapFromJSON<String, String>" {
-        forAll<String, String, String> { value1, value2, value3 ->
+        checkAll<String, String, String> { value1, value2, value3 ->
             val json = """
             {
                 "key1": ${JSONObject.quote(value1)},
@@ -97,14 +95,14 @@ class PrimitiveJSONEncodableTests : StringSpec({
             """.trimIndent()
 
             val map = mapFromJSON<String, String>(json)
-            value1 == map["key1"] &&
-                value2 == map["key2"] &&
-                value3 == map["key3"]
+            value1.shouldBe(map["key1"])
+            value2.shouldBe(map["key2"])
+            value3.shouldBe(map["key3"])
         }
     }
 
     "mapFromJSON<String, Int>" {
-        forAll<Int, Int, Int> { value1, value2, value3 ->
+        checkAll<Int, Int, Int> { value1, value2, value3 ->
             val json = """
             {
                 "key1": $value1,
@@ -114,14 +112,14 @@ class PrimitiveJSONEncodableTests : StringSpec({
             """.trimIndent()
 
             val map = mapFromJSON<String, Int>(json)
-            value1 == map["key1"] as Int &&
-                value2 == map["key2"] as Int &&
-                value3 == map["key3"] as Int
+            value1.shouldBe(map["key1"] as Int)
+            value2.shouldBe(map["key2"] as Int)
+            value3.shouldBe(map["key3"] as Int)
         }
     }
 
     "mapFromJSON<String, Any>" {
-        forAll<String, String, Int> { value1, value2, value3 ->
+        checkAll<String, String, Int> { value1, value2, value3 ->
             val json = """
             {
                 "key1": ${JSONObject.quote(value1)},
@@ -131,14 +129,14 @@ class PrimitiveJSONEncodableTests : StringSpec({
             """.trimIndent()
 
             val map = mapFromJSON<String, Any>(json)
-            value1 == map["key1"] as String &&
-                value2 == map["key2"] as String &&
-                value3 == map["key3"] as Int
+            value1.shouldBe(map["key1"] as String)
+            value2.shouldBe(map["key2"] as String)
+            value3.shouldBe(map["key3"] as Int)
         }
     }
 
     "mapFromJSON<String, String> from Int throws" {
-        forAll<String, String, Int> { value1, value2, value3 ->
+        checkAll<String, String, Int> { value1, value2, value3 ->
             val json = """
                     {
                         "key1": ${JSONObject.quote(value1)},
@@ -150,13 +148,11 @@ class PrimitiveJSONEncodableTests : StringSpec({
             shouldThrow<JSONException> {
                 mapFromJSON<String, String>(json)
             }
-
-            true
         }
     }
 
     "arrayFromJSON<String>" {
-        forAll<String, String, String> { value1, value2, value3 ->
+        checkAll<String, String, String> { value1, value2, value3 ->
             val json = """
             [
                 ${JSONObject.quote(value1)},
@@ -166,14 +162,14 @@ class PrimitiveJSONEncodableTests : StringSpec({
             """.trimIndent()
 
             val array = arrayFromJSON<String>(json)
-            value1 == array[0] &&
-                value2 == array[1] &&
-                value3 == array[2]
+            value1.shouldBe(array[0])
+            value2.shouldBe(array[1])
+            value3.shouldBe(array[2])
         }
     }
 
     "arrayFromJSON<Int>" {
-        forAll<Int, Int, Int> { value1, value2, value3 ->
+        checkAll<Int, Int, Int> { value1, value2, value3 ->
             val json = """
                 [
                     $value1,
@@ -183,14 +179,14 @@ class PrimitiveJSONEncodableTests : StringSpec({
                 """.trimIndent()
 
             val array = arrayFromJSON<Int>(json)
-            value1 == array[0] &&
-                value2 == array[1] &&
-                value3 == array[2]
+            value1.shouldBe(array[0])
+            value2.shouldBe(array[1])
+            value3.shouldBe(array[2])
         }
     }
 
     "arrayFromJSON<Any>" {
-        forAll<String, String, Int> { value1, value2, value3 ->
+        checkAll<String, String, Int> { value1, value2, value3 ->
             val json = """
             [
                 ${JSONObject.quote(value1)},
@@ -200,14 +196,14 @@ class PrimitiveJSONEncodableTests : StringSpec({
             """.trimIndent()
 
             val array = arrayFromJSON<Any>(json)
-            value1 == array[0] as String &&
-                value2 == array[1] as String &&
-                value3 == array[2] as Int
+            value1.shouldBe(array[0] as String)
+            value2.shouldBe(array[1] as String)
+            value3.shouldBe(array[2] as Int)
         }
     }
 
     "arrayFromJSON<String> from Int throws" {
-        forAll<String, String, Int> { value1, value2, value3 ->
+        checkAll<String, String, Int> { value1, value2, value3 ->
             val json = """
             [
                 ${JSONObject.quote(value1)},
@@ -219,53 +215,60 @@ class PrimitiveJSONEncodableTests : StringSpec({
             shouldThrow<JSONException> {
                 arrayFromJSON<String>(json)
             }
-
-            true
         }
     }
 
     "List<Int> toJsonEncodable" {
-        forAll<List<Int>> { a ->
+        checkAll<List<Int>> { a ->
             val jsonString = a.toJSONEncodable().encode()
             val jsonArray = JSONArray(jsonString)
-            a.indices.all { i -> a[i] == jsonArray[i] }
+            a.indices.forEach {
+                a[it].shouldBe(jsonArray[it]) }
         }
     }
 
     "List<String> toJSONEncodable.toJSONEncodable" {
-        forAll<List<String>> { a ->
+        checkAll<List<String>> { a ->
             val jsonString = a.map { it.toJSONEncodable() }.toJSONEncodable().encode()
             val jsonArray = JSONArray(jsonString)
-            a.indices.all { i -> a[i] == JSONObject(jsonArray[i] as String).get("") }
+            a.indices.forEach {
+                a[it].shouldBe(JSONObject(jsonArray[it] as String).get(""))
+            }
         }
     }
 
     "Map<String, String> toJSONEncodable" {
-        forAll<Map<String, String>> { a ->
+        checkAll<Map<String, String>> { a ->
             val jsonString = a.toJSONEncodable().encode()
             val jsonObject = JSONObject(jsonString)
-            a.entries.all { (key, value) -> value == jsonObject[key] }
+            a.entries.forEach { (key, value) ->
+                value.shouldBe(jsonObject[key])
+            }
         }
     }
 
     "Map<String, String> toJSONEncodable.toJSONEncodable" {
-        forAll<Map<String, String>> { a ->
+        checkAll<Map<String, String>> { a ->
             val jsonString = a.mapValues { it.value.toJSONEncodable() }.toJSONEncodable().encode()
             val jsonObject = JSONObject(jsonString)
-            a.entries.all { (key, value) -> value == JSONObject(jsonObject[key] as String).get("") }
+            a.entries.forEach { (key, value) ->
+                value.shouldBe(JSONObject(jsonObject[key] as String).get(""))
+            }
         }
     }
 
     "Array<Int> toJSONEncodable" {
-        forAll<List<Int>> { a ->
+        checkAll<List<Int>> { a ->
             val jsonString = a.toTypedArray().toJSONEncodable().encode()
             val jsonArray = JSONArray(jsonString)
-            a.indices.all { i -> a[i] == jsonArray[i] }
+            a.indices.forEach {
+                a[it].shouldBe(jsonArray[it])
+            }
         }
     }
 
     "List<String> toJSONEncodable.toArray.toJSONEncodable" {
-        forAll<List<String>> { a ->
+        checkAll<List<String>> { a ->
             val jsonString = a.map { it.toJSONEncodable() }.toTypedArray().toJSONEncodable().encode()
             val jsonArray = JSONArray(jsonString)
             a.indices.all { i -> a[i] == JSONObject(jsonArray[i] as String).get("") }
@@ -273,49 +276,47 @@ class PrimitiveJSONEncodableTests : StringSpec({
     }
 
     "Double toJSONEncodable" {
-        forAll<Double> { a ->
+        checkAll<Double> { a ->
             // Comparing NaN requires a different way
             // https://stackoverflow.com/questions/37884133/comparing-nan-in-kotlin
             if (a == Double.POSITIVE_INFINITY || a == Double.NEGATIVE_INFINITY || a.equals(Double.NaN as Number)) {
-                var sameExceptionMessage = false
+                var hasSameExceptionMessage = false
                 try {
                     a.toJSONEncodable()
                 } catch (e: Exception) {
-                    sameExceptionMessage = e.message.equals("Double value should be finite.")
+                    hasSameExceptionMessage = e.message.equals("Double value should be finite.")
                 }
-                sameExceptionMessage
+                hasSameExceptionMessage.shouldBeTrue()
             } else {
                 val jsonString = a.toJSONEncodable().encode()
                 val jsonObject = JSONObject(jsonString)
                 val value = jsonObject.get("")
-                if (a == value) {
-                    true
-                } else {
+                if (a != value) {
                     // If the generated value that is, for example, like 1.0, the fractional values
                     // are dropped by JSON serializer and returns a whole number.  So the check here seeks
                     // if the whole number comparison, assuming that fractional part was 0, would be equal.
                     val convertedToInt = a.toInt()
-                    convertedToInt == value ?: false
+                    convertedToInt.shouldBe(value ?: false)
                 }
             }
         }
     }
 
     "Int toJSONEncodable" {
-        forAll<Int> { a ->
+        checkAll<Int> { a ->
             val jsonString = a.toJSONEncodable().encode()
             val jsonObject = JSONObject(jsonString)
             val value = jsonObject.get("")
-            a == value
+            a.shouldBe(value)
         }
     }
 
     "Boolean toJSONEncodable" {
-        forAll<Boolean> { a ->
+        checkAll<Boolean> { a ->
             val jsonString = a.toJSONEncodable().encode()
             val jsonObject = JSONObject(jsonString)
             val value = jsonObject.get("")
-            a == value
+            a.shouldBe(value)
         }
     }
 
@@ -329,11 +330,11 @@ class PrimitiveJSONEncodableTests : StringSpec({
     }
 
     "String toJSONEncodable" {
-        forAll<String> { a ->
+        checkAll<String> { a ->
             val jsonString = a.toJSONEncodable().encode()
             val jsonObject = JSONObject(jsonString)
             val value = jsonObject.get("")
-            a == value
+            a.shouldBe(value)
         }
     }
 })
