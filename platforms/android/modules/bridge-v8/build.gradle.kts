8f437cf5259ba03d2979b281df961def239a0304
Jamie Houston <jamie.houston@salesforce.com>
2020-07-23 08:29:16 -0500

    merge

--- platforms/android/modules/bridge-v8/build.gradle.kts
+++ platforms/android/modules/bridge-v8/build.gradle.kts
@@ -33,28 +33,6 @@ dependencies {
     androidTestImplementation(Libs.espressoCore)
     androidTestImplementation(Libs.junit)
     androidTestImplementation(Libs.truth)
-//    androidTestImplementation(Libs.kotestRunnerJUnit5){
-//        exclude("io.mockk")
-//        exclude("io.github.classgraph")
-//    }
-//    androidTestImplementation(Libs.kotestCore)
-//        exclude("io.github.classgraph")
-//    }
-//    androidTestImplementation("io.kotest:kotest-extensions-robolectric-jvm:4.0.1") {
-//        exclude("org.apache.maven", "maven-artifact")
-//        exclude("org.apache.maven", "maven-artifactmanager")
-//        exclude("org.apache.maven", "maven-model")
-//        exclude("org.apache.maven", "maven-ant-tasks")
-////        exclude group: 'org.apache.maven', module: 'maven-model'
-////        exclude group: 'org.apache.maven', module: 'maven-plugin-registry'
-////        exclude group: 'org.apache.maven', module: 'maven-profile'
-////        exclude group: 'org.apache.maven', module: 'maven-project'
-////        exclude group: 'org.apache.maven', module: 'maven-settings'
-////        exclude group: 'org.apache.maven', module: 'maven-error-diagnostics'
-////        exclude group: "org.apache.maven.wagon"
-//        exclude("io.mockk")
-//        exclude("io.github.classgraph")
-//    }
 
     androidTestImplementation(Libs.kotestProperty)
     androidTestImplementation(Libs.kotestAssertions)
