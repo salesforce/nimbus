2de26a77b5f833c543746ca62eec86f234f43d31
Paul Tiarks <ptiarks@salesforce.com>
2020-08-10 14:06:55 -0500

    Move NimbusJS items over to Nimbus target

---
+++ platforms/apple/Sources/Nimbus/WebView+NimbusJS.swift
@@ -0,0 +1,34 @@
+//
+// Copyright (c) 2020, Salesforce.com, inc.
+// All rights reserved.
+// SPDX-License-Identifier: BSD-3-Clause
+// For full license text, see the LICENSE file in the repo
+// root or https://opensource.org/licenses/BSD-3-Clause
+//
+
+import WebKit
+
+enum NimbusJSError: Error {
+    case sourceNotFound
+}
+
+public extension WKWebView {
+    func injectNimbusJavascript(scriptName: String = "nimbus", bundle: Bundle? = nil) throws {
+        let bundlesToSearch: [Bundle]
+        if let bundle = bundle {
+            bundlesToSearch = [bundle]
+        } else {
+            bundlesToSearch = Bundle.allFrameworks
+        }
+        let foundPath = bundlesToSearch.compactMap { mapBundle in
+            mapBundle.path(forResource: scriptName, ofType: "js")
+        }.first
+        guard let sourcePath = foundPath else {
+            throw NimbusJSError.sourceNotFound
+        }
+
+        let source = try String(contentsOfFile: sourcePath)
+        let userScript = WKUserScript(source: source, injectionTime: .atDocumentStart, forMainFrameOnly: true)
+        configuration.userContentController.addUserScript(userScript)
+    }
+}
---
+++ platforms/apple/Sources/NimbusTests/NimbusJSTests.swift
@@ -0,0 +1,28 @@
+//
+// Copyright (c) 2020, Salesforce.com, inc.
+// All rights reserved.
+// SPDX-License-Identifier: BSD-3-Clause
+// For full license text, see the LICENSE file in the repo
+// root or https://opensource.org/licenses/BSD-3-Clause
+//
+
+import WebKit
+import XCTest
+@testable import Nimbus
+
+class NimbusJSTests: XCTestCase {
+    func testInjectingJS() {
+        let webView = WKWebView()
+        let testBundle = Bundle(for: type(of: self))
+        let userContentController = webView.configuration.userContentController
+        XCTAssertEqual(userContentController.userScripts.count, 0)
+        XCTAssertNoThrow(try webView.injectNimbusJavascript(scriptName: "testJSSource", bundle: testBundle))
+        XCTAssertEqual(userContentController.userScripts.count, 1)
+    }
+
+    func testInjectionFailure() {
+        let webView = WKWebView()
+        let testBundle = Bundle(for: type(of: self))
+        XCTAssertThrowsError(try webView.injectNimbusJavascript(scriptName: "nonexistentscript", bundle: testBundle))
+    }
+}
