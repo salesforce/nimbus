04ae994ee4ec6dbbb7679bcc9e73748f4358d925
Jamie Houston <jamie.houston@salesforce.com>
2020-10-14 06:00:39 -0500

    wip

--- platforms/android/buildSrc/src/main/kotlin/BintrayUpload.kt
+++ platforms/android/buildSrc/src/main/kotlin/BintrayUpload.kt
@@ -31,15 +31,15 @@ fun BintrayExtension.setupPublicationsUpload(
         project.checkNoVersionRanges()
     }
 
-    bintrayUpload.configure {
-        doFirst {
-            val gitTag = ProcessGroovyMethods.getText(
-                Runtime.getRuntime().exec("git describe --dirty")
-            ).trim()
-            val expectedTag = "${project.version}"
-            if (gitTag != expectedTag) error("Expected git tag '$expectedTag' but got '$gitTag'")
-        }
-    }
+//    bintrayUpload.configure {
+//        doFirst {
+//            val gitTag = ProcessGroovyMethods.getText(
+//                Runtime.getRuntime().exec("git describe --dirty")
+//            ).trim()
+//            val expectedTag = "${project.version}"
+//            if (gitTag != expectedTag) error("Expected git tag '$expectedTag' but got '$gitTag'")
+//        }
+//    }
 
     user = (project.getSettingValue("bintrayUser") ?: System.getenv("BINTRAY_USER"))
     key = (project.getSettingValue("bintrayApiKey") ?: System.getenv("BINTRAY_API_KEY"))
@@ -55,8 +55,8 @@ fun BintrayExtension.setupPublicationsUpload(
         issueTrackerUrl = project.getSettingValue(PublishingSettingsKey.issuesUrl)
         publicDownloadNumbers = true
         githubRepo = project.getSettingValue(PublishingSettingsKey.githubRepo)
-        publish = true
-        dryRun = true
+        publish = false
+        dryRun = false
         version(closureOf<BintrayExtension.VersionConfig> {
             name = project.version.toString()
         })
