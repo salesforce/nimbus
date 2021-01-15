dad2ce81ba2c97583e49394cfc8d6d5bb9a07da6
Nadeem Mohammed <nmohammed@salesforce.com>
2021-01-15 14:25:36 -0800

    Make EncodeDefaults true

--- platforms/android/modules/core/src/main/java/com/salesforce/nimbus/Constants.kt
+++ platforms/android/modules/core/src/main/java/com/salesforce/nimbus/Constants.kt
@@ -13,5 +13,5 @@ const val NIMBUS_BRIDGE = "__nimbus"
 const val NIMBUS_PLUGINS = "plugins"
 const val NIMBUS_CLASS_DISCRIMINATOR = "__type"
 @Suppress("EXPERIMENTAL_API_USAGE")
-val NIMBUS_JSON_TYPE = Json { classDiscriminator = NIMBUS_CLASS_DISCRIMINATOR }
-val NIMBUS_JSON_DEFAULT = Json { Json.Default }
+val NIMBUS_JSON_TYPE = Json { classDiscriminator = NIMBUS_CLASS_DISCRIMINATOR; encodeDefaults = true }
+val NIMBUS_JSON_DEFAULT = Json { encodeDefaults = true }
